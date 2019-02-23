d3c47d28c86282d057c0f603c73517750cb69661
yanghongwei <yanghw_117@163.com>
2019-02-23 18:54:53 +0800

    update

---
+++ src/main/java/com/it/cloud/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.it.cloud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.it.cloud.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
